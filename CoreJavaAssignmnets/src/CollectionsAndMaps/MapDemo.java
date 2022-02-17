fd7e6ff855fcb1b34d92b8d6bd908f2afdb90a49
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-02-16 16:33:42 -0800

    fixed bugs in map demo

--- CoreJavaAssignmnets/src/CollectionsAndMaps/MapDemo.java
+++ CoreJavaAssignmnets/src/CollectionsAndMaps/MapDemo.java
@@ -14,7 +14,7 @@ public class MapDemo {
 		hm.put(3, "Catie");
 
 		System.out.println("\nThe elements of Hashmap are ");
-		for (Map.Entry m : hm.entrySet()) {
+		for (Map.Entry <Integer, String> m : hm.entrySet()) {
 			System.out.println(m.getKey() + " " + m.getValue());
 		}
 
@@ -28,7 +28,7 @@ public class MapDemo {
 		ht.put(7, "John");
 
 		System.out.println("\nThe elements of HashTable are ");
-		for (Map.Entry n : ht.entrySet()) {
+		for (Map.Entry <Integer, String> n : ht.entrySet()) {
 			System.out.println(n.getKey() + " " + n.getValue());
 		}
 
@@ -40,7 +40,7 @@ public class MapDemo {
 		map.put(10, "Catie");
 
 		System.out.println("\nThe elements of TreeMap are ");
-		for (Map.Entry l : map.entrySet()) {
+		for (Map.Entry  <Integer, String> l : map.entrySet()) {
 			System.out.println(l.getKey() + " " + l.getValue());
 		}
 
